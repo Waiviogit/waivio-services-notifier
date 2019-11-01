9d481d5f76086d97c04dfc990ea7e615838602e7
Maxim Oancha <maksimoancha12@gmail.com>
2019-11-01 14:17:39 +0200

    add mongoose and eslint
    add mongoose and eslint to project, eslint config, database configs, mongoose connector, client schema

---
+++ database/notifierdb_connect.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const config = require('../config');
+
+const URI = `mongodb://${config.db.host}:${config.db.port}/${config.db.database}`;
+
+module.exports = mongoose.createConnection(URI, { useNewUrlParser: true, useFindAndModify: false },
+    () => console.log('Connect to Notifier MongoDB successful!'));

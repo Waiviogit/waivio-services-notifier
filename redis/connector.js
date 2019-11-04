42914886a8e6ae7e060b00643097a7ae87265aa9
Maxim Oancha <maksimoancha12@gmail.com>
2019-11-04 15:02:41 +0200

    change redis connectors schema

--- redis/connector.js
+++ redis/connector.js
@@ -7,13 +7,19 @@ bluebird.promisifyAll(redis.Multi.prototype);
 
 const dbClients = [];
 for (const server of config.redis.servers) {
-    const dbClient = redis.createClient(server.port, '127.0.0.1', { retry_delay: 3000 });
-    dbClient.select(config.redis.lastBlockDbNum);
-    dbClient.on('error', (err) => {
-        console.error(`Error on redis server ${server.name} which connect to port: ${server.port}`);
-        console.error(err);
-    });
-    dbClients.push(dbClient);
+    for(const db of server.databases) {
+        const dbClient = redis.createClient(server.port, '127.0.0.1', { retry_delay: 3000 });
+        dbClient.select(db.db_num);
+        dbClient.lastBlockKeys = db.keys;
+        dbClient.on('error', (err) => {
+            console.error(`Error on redis server ${server.name} which connect to port: ${server.port} on DB: ${db.db_num}`);
+            console.error(err);
+        });
+        dbClients.push(dbClient);
+    }
 }
+
+console.log(`${dbClients.length} Redis databases successful connected!`);
+
 
 module.exports = { dbClients };

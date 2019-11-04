ec5264badaccfb08b17e926fd0ccfb53975b7daf
Maxim Oancha <maksimoancha12@gmail.com>
2019-11-04 14:18:44 +0200

    fix async-await on command callbacks

--- telegram/commands.js
+++ telegram/commands.js
@@ -3,34 +3,34 @@ const { nodeUrls, NotifiesType } = require('../config');
 const views = require('./views');
 const { clients } = require('../services');
 
-app.command('start', (ctx) => {
+app.command('start', async (ctx) => {
     console.log(`Command "/start" from ${ctx.from.id} in chat ${ctx.chat.id}`);
-    ctx.replyWithMarkdown(views.START_MESSAGE);
+    await ctx.replyWithMarkdown(views.START_MESSAGE);
 });
 
-app.command('nodes', (ctx) => {
+app.command('nodes', async (ctx) => {
     console.log(`Command "/nodes" from ${ctx.from.id} in chat ${ctx.chat.id}`);
-    ctx.replyWithMarkdown(JSON.stringify(nodeUrls));
+    await ctx.replyWithMarkdown(JSON.stringify(nodeUrls));
 });
 
-app.command('help', (ctx) => {
+app.command('help', async (ctx) => {
     console.log(`Command "/help" from ${ctx.from.id} in chat ${ctx.chat.id}`);
-    ctx.replyWithMarkdown(views.HELP_MESSAGE);
+    await ctx.replyWithMarkdown(views.HELP_MESSAGE);
 });
 
 app.command('subNotifications', async (ctx) => {
     console.log(`Command "/subNotifications" from ${ctx.from.id} in chat ${ctx.chat.id}`);
     const { result, error } = await clients.updateSubscribedNotifies({ client_id: ctx.chat.id, subscribedNotifies: [ ...NotifiesType ] });
     if(result.ok) {
-        ctx.replyWithMarkdown(views.SUB_NOTIFICATIONS_MESSAGE);
+        await ctx.replyWithMarkdown(views.SUB_NOTIFICATIONS_MESSAGE);
     }else{
-        ctx.replyWithMarkdown('Something went wrong!');
+        await ctx.replyWithMarkdown('Something went wrong!');
         console.error(`Error on "subNotifications" from chat ${ctx.chat.id}`);
         console.error(error);
     }
 });
 
-app.on('sticker', (ctx) => {
+app.on('sticker', async (ctx) => {
     console.log(`"Sticker" from ${ctx.from.id} in chat ${ctx.chat.id}`);
-    ctx.replyWithMarkdown('hehe boayy');
+    await ctx.replyWithMarkdown('hehe boayy');
 });

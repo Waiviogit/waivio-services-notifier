6d4d3843badefbc23135eff48e0cc668ef7d5c4e
Maksim <maksimoancha12@gmail.com>
2020-02-18 17:15:01 +0200

    add new docs links

--- telegram/views.js
+++ telegram/views.js
@@ -2,11 +2,13 @@ exports.HELP_MESSAGE = [ '/help - show this page',
     '/start - start bot',
     '/nodes - show active nodes list',
     '/subNotifications - subscribe for services fall notifications',
-    '/status - current status of waivio services' ].join('\n');
+    '/status - current status of Waivio services',
+    '/apilinks - list of Waivio docs' ].join('\n');
 exports.START_MESSAGE = 'Welcome to waivio notify bot!\nUse /help to show info.';
 exports.SUB_NOTIFICATIONS_MESSAGE = 'Now you\'ll be notified when one of services has fall down';
 exports.APILINKS_MESSAGE = [ 'WAIVIO Docs links:',
     ' - https://app.swaggerhub.com/apis-docs/waivio/waivio-api/1.0.0',
     ' - https://app.swaggerhub.com/apis/waivio/investarena-api/1.0.0',
-    ' - https://app.swaggerhub.com/apis/waivio/objects-bot/1.0.0' ].join('\n');
+    ' - https://app.swaggerhub.com/apis/waivio/objects-bot/1.0.0',
+    ' - https://app.swaggerhub.com/apis-docs/waiviogit/waivio-admin-api/1.0.0' ].join('\n');
 

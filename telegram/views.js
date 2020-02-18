f400939a61e5926b2d988997bf07ec372b79e8e5
Maksim <maksimoancha12@gmail.com>
2020-02-18 17:17:01 +0200

    fix api links

--- telegram/views.js
+++ telegram/views.js
@@ -8,7 +8,7 @@ exports.START_MESSAGE = 'Welcome to waivio notify bot!\nUse /help to show info.'
 exports.SUB_NOTIFICATIONS_MESSAGE = 'Now you\'ll be notified when one of services has fall down';
 exports.APILINKS_MESSAGE = [ 'WAIVIO Docs links:',
     ' - https://app.swaggerhub.com/apis-docs/waivio/waivio-api/1.0.0',
-    ' - https://app.swaggerhub.com/apis/waivio/investarena-api/1.0.0',
-    ' - https://app.swaggerhub.com/apis/waivio/objects-bot/1.0.0',
+    ' - https://app.swaggerhub.com/apis-docs/waivio/investarena-api/1.0.0',
+    ' - https://app.swaggerhub.com/apis-docs/waivio/objects-bot/1.0.0',
     ' - https://app.swaggerhub.com/apis-docs/waiviogit/waivio-admin-api/1.0.0' ].join('\n');
 

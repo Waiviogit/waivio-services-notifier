68974a25fba437d174fbcd0dad2f5af5f963a0e4
olegtsigulov <olegcigulev@gmail.com>
2020-03-25 10:02:02 +0200

    add import queue notifier

---
+++ redis/rsmq/index.js
@@ -0,0 +1,6 @@
+const config = require('../../config');
+const RedisSMQ = require('rsmq');
+const importRsmqClient = new RedisSMQ(
+    { ns: 'rsmq', options: { host: '127.0.0.1', port: config.redis.importServer.port, db: config.redis.importServer.db_num } });
+
+module.exports = { importRsmqClient };

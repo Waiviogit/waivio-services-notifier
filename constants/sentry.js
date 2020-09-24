2a866a012c5f4581bffb10ebffa890e821d80eb9
olegtsigulov <olegcigulev@gmail.com>
2020-09-24 11:45:01 +0300

    add waivio app to sentry

--- constants/sentry.js
+++ constants/sentry.js
@@ -5,5 +5,6 @@ exports.APP_DATA = {
     'waivioCampaigns': 5433370,
     'notificationsApi': 5438646,
     'nodeParser': 5438673,
-    'objectsBot': 5438751
+    'objectsBot': 5438751,
+    'waivio': 5439695
 };

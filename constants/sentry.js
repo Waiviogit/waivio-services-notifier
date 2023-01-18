34dd7d32dd900e2259e57e54ce28b2237de7474e
fesmofet <borees2@gmail.com>
2023-01-18 16:30:53 +0200

    engineNodeParser add

--- constants/sentry.js
+++ constants/sentry.js
@@ -6,5 +6,6 @@ exports.APP_DATA = {
     'notificationsApi': 5438646,
     'nodeParser': 5438673,
     'objectsBot': 5438751,
-    'waivio': 5439695
+    'waivio': 5439695,
+    'engineNodeParser': 4504525930889216,
 };
